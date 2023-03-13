c561fb6c890ee33d3cc8b61224fb97a7841cf912
Renan Dante <renandantearaujo@gmail.com>
2023-03-13 20:08:12 -0300

    Primeiro commit do projeto

---
+++ src/components/Form/style.js
@@ -0,0 +1,34 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    form: {
+        backgroundColor: '#fff',
+        padding: 30,
+        height: '100%',
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+    label: {
+        fontSize: 18,
+        textAlign: 'center',
+        marginBottom: 10,
+    },
+    input: {
+        fontSize: 20,
+        borderRadius: 10,
+        backgroundColor: '#ddd',
+        padding: 10,
+        marginBottom: 20,
+    },
+    buttom: {
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center',
+        backgroundColor: '#f90',
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+});
+
+export default styles
